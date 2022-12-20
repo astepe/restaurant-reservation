aab185592215ea37849f279d5c6bfedc95797d72
astepe <arisstepe@gmail.com>
2022-12-19 18:01:33 -0600

    update config, remove unneeded files

--- cluster/terraform/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-
-def lambda_handler(event, context):
-    instance_id = event["instance_id"]
-    if event["action"] == "hibernate":
-        boto3.client("ec2").stop_instances(InstanceIds=[instance_id], Hibernate=True)
-    return instance_id
