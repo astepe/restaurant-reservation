bb7e3023227f7b865b7d2bfa46e52c8ade70edd5
astepe <arisstepe@gmail.com>
2023-01-10 20:58:17 -0500

    Add test case and test runner to cicd

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,10 +2,25 @@
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, Bean!"
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::index)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
 }
